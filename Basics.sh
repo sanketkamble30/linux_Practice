974a69013a3807760f9e9e471b2c686147838937
sanketkamble30 <ec2-user@ip-172-31-34-18.eu-north-1.compute.internal>
2025-07-19 09:23:12 +0000

    Add git Basic commands Script

---
+++ Basics.sh
@@ -0,0 +1,20 @@
+echo "== My Linux Practice Script =="
+
+pwd              #check your current path/location
+whoami           #display the current logged in user
+date             #show date and time 
+date +%D         #only date
+date +%H:%M      #only time
+ls               #shows file & folder
+ls -lt           #shows an detail information 
+ls -ltr          #shows latest file or edited files
+clear            #clear the terminal
+cat filename     #display the content of file
+less filename    #read a file and search for word
+mkdir foldername #create an directory
+touch filename   #create an file
+rm filename      #delete a file
+rmdir foldername #delete a folder
+vi filename      #edit the file
+nano filename    #add any line in file
+
