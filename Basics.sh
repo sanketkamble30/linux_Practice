6e3959f562efbded067be94978f69d9b37bcaeb0
sanketkamble30 <ec2-user@ip-172-31-34-18.eu-north-1.compute.internal>
2025-07-20 11:01:24 +0000

    Updated Basics.sh with new commands

--- Basics.sh
+++ Basics.sh
@@ -1,5 +1,3 @@
-echo "== My Linux Practice Script =="
-
 pwd              #check your current path/location
 whoami           #display the current logged in user
 date             #show date and time 
@@ -12,9 +10,18 @@ clear            #clear the terminal
 cat filename     #display the content of file
 less filename    #read a file and search for word
 mkdir foldername #create an directory
+mkdir prject{1..5} #create an multiple directory
+rmdir foldername #delete the directory
+rm -r            #delete the non empty directory
+mkdir ../../     #go for the previous directory
 touch filename   #create an file
 rm filename      #delete a file
 rmdir foldername #delete a folder
 vi filename      #edit the file
-nano filename    #add any line in file
+nano filename    #add any line in file or edit file
+cp filenameA filename2 #copy one file to another
+mv filename foldername #cut-paste a file from a another folder
+mv fileA filename      #rename the file one to another
+head -5 filename       #read or display the top 5 lines from the files
+tail -5 filename       #display or read the bottam 5 linese form a linux
 
