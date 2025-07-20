66b148819eccb3b9bda07c6fb56addb58fc3eb8e
sanketkamble30 <ec2-user@ip-172-31-34-18.eu-north-1.compute.internal>
2025-07-20 12:10:51 +0000

    Add SystemInfo.sh with hardware and OS info commands

---
+++ systeminfo.sh
@@ -0,0 +1,9 @@
+tty                #display the terminal type and number
+uname              #shows operating system name
+uname -a           #shows kernal information
+hostnamectl        #shows the host detailed information about system
+cat /proc/cpuinfo   # CPU details
+cat /proc/meminfo   # Memory details
+df -h               # Disk space
+free -h             # RAM usage
+
