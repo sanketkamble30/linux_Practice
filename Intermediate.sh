f468707b86da9638c438515ccf41ec5285ed5fd4
sanketkamble30 <ec2-user@ip-172-31-34-18.eu-north-1.compute.internal>
2025-07-20 11:56:06 +0000

    Add Intermediate.sh with more Linux commands

---
+++ Intermediate.sh
@@ -0,0 +1,19 @@
+sort filename | uniq      #display the unique content from a file
+split -l 3 filename       #split the content in line in group of 3 lines
+grep "word" filename      #search a word and display the matching contant from the file
+egrep "word 1 | word2"    #search the multiple word and display the matching content from a file
+shuf filename             #shuffle the content in file
+wc -l filename            #count the no.of files in linux
+cmp filename1 filename2   #check if two files are identical or not
+diff -u filename1 filename2 #compare and display the diff bet two files
+*.filename                  #find the file
+cal                         #display the calender
+cal 2025                    #display the all year calender
+sudo su                     #login as root user
+sudo -i                     
+tty                         #display the terminal type and number
+history                     #display the previously used commands in past
+help                        #check syntax and options available for a command
+man commandname             #get the more information about the command  eg- man ls
+bc                          #use the calculator in linux
+
